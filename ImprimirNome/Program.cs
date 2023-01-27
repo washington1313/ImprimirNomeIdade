a671ff07a9cfa6a8a2c44d14a1a9b6e43c455ab2
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-27 15:36:48 -0300

    Projeto Finalizado

---
+++ ImprimirNome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ImprimirNome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe seu nome: ");
+            string EntradaNome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua idade: ");
+            int EntradaIdade = int.Parse(Console.ReadLine());
+
+            ImprimirTextoDados(EntradaNome, EntradaIdade);
+
+            Console.ReadKey();
+        }
+
+        static void ImprimirTextoDados(string nome, int idade)
+        {
+            Console.WriteLine("Meu nome é: " + nome + ",e tenho " + idade + " anos de idade.");
+        }
+
+    }
+}
